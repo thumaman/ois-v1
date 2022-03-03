dbd3531f590dd5893ba2cea61601b99b50e60561
Vid Thuma <vt0138@student.uni-lj.si>
2022-03-03 21:05:34 +0100

    Dodan izpis

--- Deljitelji.java
+++ Deljitelji.java
@@ -4,19 +4,7 @@ public class Deljitelji {
 
     public static void main(String[] args){
 
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        
-
-        for(int i = 1; i < n; i++){
-
-            for(int j = i + 1; j <= n; j++){
-                System.out.printf("%d:%d", i,j);
- 
-        }
-            System.out.println();
-    }
+        System.out.println("ois je kren");
 
             }
         }
